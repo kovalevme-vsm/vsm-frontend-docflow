797f502506ba413c4b6eefcbeb8788274dc500c5
Максим Евгеньевич Ковалев <kovalevme@vsm2stl.ru>
2025-05-15 15:03:55 +0300

    chore(ui): уведомление о том что нельзя редактировать список групп

--- src/widgets/groups-management-tab/ui/groups-management-tab.tsx
+++ src/widgets/groups-management-tab/ui/groups-management-tab.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import { IoIosWarning } from 'react-icons/io';
 
 import { useListGroupsManagement } from 'widgets/groups-management-tab/api/use-list-groups-management.ts';
 import { TABLE_COLUMNS } from 'widgets/groups-management-tab/models/table-columns.tsx';
@@ -11,6 +12,10 @@ export function GroupsManagementTab(): ReactElement {
   return (
     <div className={'space-y-4'}>
       <Search />
+      <span className={'flex items-center gap-2 text-sm font-medium text-yellow-500'}>
+        <IoIosWarning className={'text-lg'} />
+        Список системных групп пользователей, которые невозможно создать и отредактировать.
+      </span>
       <Table
         columns={TABLE_COLUMNS}
         loading={isPendingGroups}
