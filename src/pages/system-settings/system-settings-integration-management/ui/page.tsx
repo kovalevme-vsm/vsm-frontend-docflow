b1e6622a708f84a9324999d0b3d9c5de9427a86c
Максим Евгеньевич Ковалев <kovalevme@vsm2stl.ru>
2025-05-20 18:02:13 +0300

    chore(ui): Конструктор маршрута. ver 3

--- src/pages/system-settings/system-settings-integration-management/ui/page.tsx
+++ src/pages/system-settings/system-settings-integration-management/ui/page.tsx
@@ -1,16 +1,96 @@
+import { Button } from 'antd';
 import { ReactElement } from 'react';
-import { TbApi } from 'react-icons/tb';
+import { TbApi, TbSettings } from 'react-icons/tb';
 
 import { SectionHeader } from 'entities/section-header';
 
+import firstCCompany from '../assets/1C.svg';
+import ad from '../assets/AD.svg';
+import diadoc from '../assets/diadoc.svg';
+import extern from '../assets/extern.svg';
+
 export default function SystemSettingsIntegrationManagement(): ReactElement {
   return (
-    <SectionHeader
-      icon={TbApi}
-      title={'Интеграции и API'}
-      description={
-        'Подключайте новые сервисы, управляйте и просматривайте журналы интеграций'
-      }
-    />
+    <div>
+      <SectionHeader
+        icon={TbApi}
+        title={'Интеграции и API'}
+        description={'Подключайте новые сервисы, управляйте и просматривайте журналы интеграций'}
+      />
+      <div className={'grid grid-cols-3 gap-4'}>
+        <div className={'flex items-center justify-between rounded-2xl bg-white p-6'}>
+          <div className={'space-y-2'}>
+            <div
+              className={
+                'flex aspect-square w-12 items-center justify-center rounded-xl border border-gray-200 bg-white p-2 shadow-lg/5'
+              }
+            >
+              <img src={ad} />
+            </div>
+            <div className={'flex flex-col'}>
+              <span className={'font-semibold'}>Active Directory</span>
+              <span className={'text-xs'}>Справочник пользователей</span>
+            </div>
+          </div>
+          <div>
+            <Button icon={<TbSettings />} />
+          </div>
+        </div>
+        <div className={'flex items-center justify-between rounded-2xl bg-white p-6'}>
+          <div className={'space-y-2'}>
+            <div
+              className={
+                'flex aspect-square w-12 items-center justify-center rounded-xl border border-gray-200 bg-white p-2 shadow-lg/5'
+              }
+            >
+              <img src={firstCCompany} />
+            </div>
+            <div className={'flex flex-col'}>
+              <span className={'font-semibold'}>1C:Предприятие</span>
+              <span className={'text-xs'}>Интеграция с 1C:Бухгалтерия</span>
+            </div>
+          </div>
+          <div>
+            <Button icon={<TbSettings />} />
+          </div>
+        </div>
+        <div className={'flex items-center justify-between rounded-2xl bg-white p-6'}>
+          <div className={'space-y-2'}>
+            <div
+              className={
+                'flex aspect-square w-12 items-center justify-center rounded-xl border border-gray-200 bg-white p-2 shadow-lg/5'
+              }
+            >
+              <img src={diadoc} />
+            </div>
+            <div className={'flex flex-col'}>
+              <span className={'font-semibold'}>Контур Диадок</span>
+              <span className={'text-xs'}>Интеграция ЭДО</span>
+            </div>
+          </div>
+          <div>
+            <Button icon={<TbSettings />} />
+          </div>
+        </div>
+        <div className={'flex items-center justify-between rounded-2xl bg-white p-6'}>
+          <div className={'space-y-2'}>
+            <div
+              className={
+                'flex aspect-square w-12 items-center justify-center rounded-xl border border-gray-200 bg-white p-2 shadow-lg/5'
+              }
+            >
+              <img src={extern} />
+            </div>
+            <div className={'flex flex-col'}>
+              <span className={'font-semibold'}>Контур Экстерн</span>
+              <span className={'text-xs'}>Интеграция ЭДО</span>
+            </div>
+          </div>
+          <div>
+            <Button icon={<TbSettings />} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/pages/system-settings/system-settings-route-management-create/ui/page.tsx
+++ src/pages/system-settings/system-settings-route-management-create/ui/page.tsx
@@ -1,14 +1,15 @@
-import { Button, Divider, Form } from 'antd';
-import { ReactElement } from 'react';
+import { Button, Divider, Form, Input, Modal, Radio } from 'antd';
+import { Fragment, ReactElement } from 'react';
+import { FiSave } from 'react-icons/fi';
 import { IoFootsteps } from 'react-icons/io5';
 import { RiInfoCardLine } from 'react-icons/ri';
 import { TbPlus } from 'react-icons/tb';
 
-import { RouteBaseForm } from 'pages/system-settings/system-settings-route-management-create/ui/route-forms/route-base-form.tsx';
-
 import { Empty } from 'entities/empty';
 import { SectionHeader } from 'entities/section-header';
+import SelectInfinite from 'entities/select-infinite';
 
+import { QUERY } from 'shared/const';
 import { Label } from 'shared/ui';
 
 export default function SystemSettingsRouteManagementCreate(): ReactElement {
@@ -19,11 +20,59 @@ export default function SystemSettingsRouteManagementCreate(): ReactElement {
         title={'Создание нового маршрута'}
         description={'Создавайте, редактируйте, удаляйте маршруты для каждого типа документов'}
       />
-      <Form>
+      <Form
+        onFinish={(values) => {
+          Modal.confirm({
+            title: 'Вы уверены, что хотите создать маршрут без единого шага?',
+            content: 'При подтверждении действия, маршрут будет создан, как неактивный.',
+            centered: true,
+            okText: 'Подтвердить',
+            onOk: () => {
+              values.is_active = false;
+              console.log(values);
+            },
+          });
+        }}
+      >
         <div className={'flex gap-2'}>
           <div className={'flex-1'}>
             <Label title={'Общая информация о маршруте'} icon={RiInfoCardLine} />
-            <RouteBaseForm />
+            <Form.Item name={'name'} rules={[{ required: true, message: 'Пожалуйста, укажите имя маршрута' }]}>
+              <Input placeholder={'Название маршрута'} allowClear />
+            </Form.Item>
+            <Form.Item name="is_active" initialValue={true}>
+              <Radio.Group
+                block
+                options={[
+                  { label: 'Вкл.', value: true },
+                  { label: 'Выкл.', value: false },
+                ]}
+                optionType="button"
+                buttonStyle="solid"
+              />
+            </Form.Item>
+            <Form.Item name={'document_type'} rules={[{ required: true, message: 'Пожалуйста, укажите тип маршрута' }]}>
+              <SelectInfinite
+                apiPath={QUERY.SYSTEM_SELECT_DOCUMENT_TYPES.paths.index}
+                queryKey={QUERY.SYSTEM_SELECT_DOCUMENT_TYPES.keys.list}
+                placeholder={'Тип маршрута'}
+                allowClear
+              />
+            </Form.Item>
+            <Form.Item name={'description'}>
+              <Input.TextArea
+                placeholder={'Описание маршрута'}
+                rows={3}
+                showCount
+                maxLength={100}
+                style={{ resize: 'none' }}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type={'primary'} icon={<FiSave />} htmlType={'submit'}>
+                Сохранить
+              </Button>
+            </Form.Item>
           </div>
           <Divider type={'vertical'} className={'!h-auto'} />
           <div className={'flex-1'}>
@@ -33,6 +82,15 @@ export default function SystemSettingsRouteManagementCreate(): ReactElement {
                 Добавить шаг
               </Button>
             </div>
+            <Form.List name={'steps'} initialValue={[]}>
+              {(fields) =>
+                fields.map((field) => (
+                  <Fragment key={field.key}>
+                    <span>{field.name}</span>
+                  </Fragment>
+                ))
+              }
+            </Form.List>
             <Empty />
           </div>
         </div>
